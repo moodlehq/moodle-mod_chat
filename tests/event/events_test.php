f7b29367b2c505f30eae41cd5ea01cb8dd879bce
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-12-14 15:04:15 +0100

    MDL-73476 phpunit: events, external and search tescase names
    All events_test, external_test and search_test classes:
    
    - Namespaced with component[\level2-API]
    - Moved to level2-API subdirectory when required.
    - Fixed incorrect use statements with leading backslash.
    - Changed code to point to global scope when needed.
    - Fix some relative paths and comments here and there.
    - All them passing individually.
    - Complete runs passing too.
    
    Special mention to lib/external/tests/external_test.php:
    - external is not a valid component
    - hence, its only valid namespace is "core"
    - also, it's testing lib/external/externallib.php
    - hence, the file (and class) have been renamed to external_externallib_test.php
      (to avoid conflicts with other external_test that may exist in core)

--- tests/event/events_test.php
+++ tests/event/events_test.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace mod_chat\event;
+
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
@@ -34,7 +36,7 @@ require_once($CFG->dirroot . '/mod/chat/lib.php');
  * @copyright  2013 Frédéric Massart
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class mod_chat_events_testcase extends advanced_testcase {
+class events_test extends \advanced_testcase {
 
     public function test_message_sent() {
         global $DB;
@@ -111,7 +113,7 @@ class mod_chat_events_testcase extends advanced_testcase {
         $chat = $this->getDataGenerator()->create_module('chat', array('course' => $course->id));
 
         $params = array(
-            'context' => context_module::instance($chat->cmid),
+            'context' => \context_module::instance($chat->cmid),
             'objectid' => $chat->id,
             'other' => array(
                 'start' => 1234,
@@ -126,7 +128,7 @@ class mod_chat_events_testcase extends advanced_testcase {
         $event = reset($events);
         $this->assertInstanceOf('\mod_chat\event\sessions_viewed', $event);
         $this->assertEquals($USER->id, $event->userid);
-        $this->assertEquals(context_module::instance($chat->cmid), $event->get_context());
+        $this->assertEquals(\context_module::instance($chat->cmid), $event->get_context());
         $this->assertEquals(1234, $event->other['start']);
         $this->assertEquals(5678, $event->other['end']);
         $this->assertEquals($chat->id, $event->objectid);
@@ -146,7 +148,7 @@ class mod_chat_events_testcase extends advanced_testcase {
         $course = $this->getDataGenerator()->create_course();
 
         $params = array(
-            'context' => context_course::instance($course->id)
+            'context' => \context_course::instance($course->id)
         );
         $event = \mod_chat\event\course_module_instance_list_viewed::create($params);
         $sink = $this->redirectEvents();
@@ -155,7 +157,7 @@ class mod_chat_events_testcase extends advanced_testcase {
         $event = reset($events);
         $this->assertInstanceOf('\mod_chat\event\course_module_instance_list_viewed', $event);
         $this->assertEquals($USER->id, $event->userid);
-        $this->assertEquals(context_course::instance($course->id), $event->get_context());
+        $this->assertEquals(\context_course::instance($course->id), $event->get_context());
         $expected = array($course->id, 'chat', 'view all', "index.php?id=$course->id", '');
         $this->assertEventLegacyLogData($expected, $event);
         $this->assertEventContextNotUsed($event);
@@ -167,7 +169,7 @@ class mod_chat_events_testcase extends advanced_testcase {
         $course = $this->getDataGenerator()->create_course();
         $chat = $this->getDataGenerator()->create_module('chat', array('course' => $course->id));
         $cm = get_coursemodule_from_instance('chat', $chat->id);
-        $context = context_module::instance($cm->id);
+        $context = \context_module::instance($cm->id);
 
         $params = array(
             'objectid' => $chat->id,
@@ -180,7 +182,7 @@ class mod_chat_events_testcase extends advanced_testcase {
         $expected = array($course->id, 'chat', 'view', "view.php?id=$cm->id", $chat->id, $cm->id);
         $this->assertEventLegacyLogData($expected, $event);
         $this->assertEventContextNotUsed($event);
-        $url = new moodle_url('/mod/chat/view.php', array('id' => $cm->id));
+        $url = new \moodle_url('/mod/chat/view.php', array('id' => $cm->id));
         $this->assertEquals($url, $event->get_url());
         $event->get_name();
     }
